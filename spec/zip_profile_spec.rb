1fa94d9b4af43972a8822a65028f89eac7bdf391
mechamogera <mechamogera@gmail.com>
2013-01-30 09:59:43 +0900

    added test to zip_profile_spec.rb

--- spec/zip_profile_spec.rb
+++ spec/zip_profile_spec.rb
@@ -15,7 +15,7 @@ describe EnvKonf::ZipProfile do
     md5 = "test"
     profile = "test/config.yaml"
 
-    Digest::MD5.should_receive(:file).with(source).and_return(md5)
+    Digest::MD5.should_receive(:file).twice.with(source).and_return(md5)
 
     mkdir_p_original = FileUtils.method(:mkdir_p)
     FileUtils.should_receive(:mkdir_p).with(
@@ -27,7 +27,40 @@ describe EnvKonf::ZipProfile do
       new_original.call(profile)
     }
 
-    EnvKonf::ZipProfile.save_encode_md5("test.yml")
+    EnvKonf::ZipProfile.save_encode_md5(source)
     YAML.load_file(profile).should == { :encode_md5 => md5 }
+
+    YAML.should_receive(:load_file).twice.with(EnvKonf::ZipProfile::FILE).and_return({ :encode_md5 => md5 })
+    EnvKonf::ZipProfile.match_encoded?(source).should be_true
+
+    Digest::MD5.should_receive(:file).with(__FILE__).and_call_original
+    EnvKonf::ZipProfile.match_encoded?(__FILE__).should be_false
+  end
+
+  it "should save decode md5" do
+    source = "test.zip"
+    md5 = "test"
+    profile = "test/config.yaml"
+
+    Digest::MD5.should_receive(:file).twice.with(source).and_return(md5)
+
+    mkdir_p_original = FileUtils.method(:mkdir_p)
+    FileUtils.should_receive(:mkdir_p).with(
+      File.dirname(EnvKonf::ZipProfile::FILE)
+    ) { mkdir_p_original.call(File.dirname(profile)) }
+    
+    new_original = YAML::Store.method(:new)
+    YAML::Store.should_receive(:new).with(EnvKonf::ZipProfile::FILE) {
+      new_original.call(profile)
+    }
+
+    EnvKonf::ZipProfile.save_decode_md5(source)
+    YAML.load_file(profile).should == { :decode_md5 => md5 }
+    
+    YAML.should_receive(:load_file).twice.with(EnvKonf::ZipProfile::FILE).and_return({ :decode_md5 => md5 })
+    EnvKonf::ZipProfile.match_decoded?(source).should be_true
+
+    Digest::MD5.should_receive(:file).with(__FILE__).and_call_original
+    EnvKonf::ZipProfile.match_decoded?(__FILE__).should be_false
   end
 end
